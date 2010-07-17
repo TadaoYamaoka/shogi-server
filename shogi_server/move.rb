cd3b4625cb8625594744999cbac91e0001368392
daigo <beatles@users.sourceforge.jp>
2010-07-17 20:31:35 +0900

    * [shogi-server]   - shogi_server.rb, shogi_server/board.rb, shogi_server/move.rb     - Refactoring: Board can now move_to() and move_back() a move       instread of deep_copy().

---
+++ shogi_server/move.rb
@@ -0,0 +1,48 @@
+## $Id$
+
+## Copyright (C) 2004 NABEYA Kenichi (aka nanami@2ch)
+## Copyright (C) 2007-2008 Daigo Moriwaki (daigo at debian dot org)
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+module ShogiServer # for a namespace
+
+class Move
+  def initialize(x0, y0, x1, y1, name, sente)
+    @x0 = x0
+    @y0 = y0
+    @x1 = x1
+    @y1 = y1
+    @name = name
+    @sente = sente
+    @promotion = false
+    @captured_piece = nil
+    @captured_piece_promoted = false
+  end
+  attr_reader :x0, :y0, :x1, :y1, :name, :sente, 
+              :captured_piece, :captured_piece_promoted
+  attr_accessor :promotion
+
+  def set_captured_piece(piece)
+    @captured_piece = piece
+    @captured_piece_promoted = piece.promoted
+  end
+
+  def is_drop?
+    return (@x0 == 0 || @y0 == 0)
+  end
+end
+
+end # namespace
---
+++ test/TC_move.rb
@@ -0,0 +1,46 @@
+$:.unshift File.join(File.dirname(__FILE__), "..")
+
+require 'test/unit'
+require 'shogi_server'
+require 'shogi_server/move'
+
+class TestMove < Test::Unit::TestCase
+
+  def test_is_drop
+    m = ShogiServer::Move.new 6,7,8,9,"FU",true
+    assert 6, m.x0
+    assert 7, m.y0
+    assert 8, m.x1
+    assert 9, m.y1
+    assert_equal "FU", m.name
+    assert m.sente
+
+    assert !m.promotion
+    assert_nil m.captured_piece
+    assert !m.captured_piece_promoted
+    assert !m.is_drop?
+
+    m = ShogiServer::Move.new 0,0,7,6,"FU",true
+    assert m.is_drop?
+  end
+
+  def test_set_captured_piece_not_promoted
+    m = ShogiServer::Move.new 2,4,2,3,"TO",true
+    board = ShogiServer::Board.new # dummy
+    fu = ShogiServer::PieceFU.new(board, 2, 3, false, false)
+    m.set_captured_piece(fu)
+
+    assert_equal fu, m.captured_piece
+  end
+
+  def test_set_captured_piece_promoted
+    m = ShogiServer::Move.new 2,4,2,3,"TO",true
+    board = ShogiServer::Board.new # dummy
+    fu = ShogiServer::PieceFU.new(board, 2, 3, false, true)
+    m.set_captured_piece(fu)
+
+    assert_equal fu, m.captured_piece
+    assert m.captured_piece_promoted
+  end
+end
+
